57245495373b4b6ed810d1fb6a64e20a0666c77d
assilbekov <askhat.assilbekov@gmail.com>
2024-05-12 13:20:11 -0300

    implement snapshot

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package momento
+package main
+
+import (
+	"fmt"
+	"momento/pkg"
+)
+
+func main() {
+	storage := &pkg.Guardian{
+		Items: make([]*pkg.Snapshot, 0),
+	}
+	creator := &pkg.Creator{
+		State: "state1",
+	}
+	fmt.Println("Creator state:", creator.GetState())
+	storage.Add(creator.Create())
+
+	creator.SetState("state2")
+	fmt.Println("Creator state:", creator.GetState())
+	storage.Add(creator.Create())
+
+	creator.Restore(storage.Get(0))
+	fmt.Println("Creator state:", creator.GetState())
+	creator.Restore(storage.Get(1))
+	fmt.Println("Creator state:", creator.GetState())
+}
